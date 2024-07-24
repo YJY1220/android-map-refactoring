079eaf777238442db5d5cae39459266166cd5a7f
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-07-24 16:16:20 +0900

    end-0724

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,6 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.0")
     implementation("com.google.dagger:hilt-android:2.46.1")
     kapt("com.google.dagger:hilt-compiler:2.46.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -90,6 +89,8 @@ dependencies {
     androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
 }
 
-kapt {
-    correctErrorTypes = true
+afterEvaluate {
+    kapt {
+        correctErrorTypes = true
+    }
 }
--- app/src/main/java/campus/tech/kakao/map/model/MapItem.kt
+++ app/src/main/java/campus/tech/kakao/map/model/MapItem.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "MapItem")
 data class MapItem(
-    @PrimaryKey val id: String,
+    @PrimaryKey val id: Int,
     val place_name: String,
     val road_address_name: String,
     val category_group_name: String,
